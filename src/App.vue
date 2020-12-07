9ec8fcb606fd306c201085a80d88c962ecc66f2a
wangdingwei <543669046@qq.com>
2020-12-07 18:05:34 +0800

    移动到相应子页面滚动条出现

--- src/App.vue
+++ src/App.vue
@@ -19,5 +19,32 @@
 </script>
 
 <style>
-
+    body {
+        overflow-y: hidden
+    }
+    /*修改滚动条样式*/
+    
+    div::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+        /**/
+    }
+    
+    div::-webkit-scrollbar-track {
+        /* background: rgb(239, 239, 239); */
+        border-radius: 2px;
+    }
+    
+    div::-webkit-scrollbar-thumb {
+        background: rgb(191, 191, 191);
+        border-radius: 10px;
+    }
+    
+    div::-webkit-scrollbar-thumb:hover {
+        background: rgb(173, 171, 171);
+    }
+    
+    div::-webkit-scrollbar-corner {
+        background: #179a16;
+    }
 </style>
--- src/views/musicHome/findMusic/findMusic.vue
+++ src/views/musicHome/findMusic/findMusic.vue
@@ -6,7 +6,7 @@
   </div>
   <div>
     <!-- 二级路由 -->
-    <router-view/>
+    <router-view id="secondRoute"/>
   </div>
   </div>
   
@@ -31,5 +31,18 @@
     #headTab {
         /* width: 100%; */
         margin-left: 30%;
+    }
+    
+    #secondRoute:hover {
+        overflow-y: overlay;
+    }
+    
+    #secondRoute {
+        padding-right: 17px;
+        padding-left: 3px;
+        height: 80vh;
+        width: 100%;
+        overflow-y: hidden;
+        overflow-x: hidden;
     }
 </style>
